801ba3d5ce9e15ce2215e3fd15b02e62ff7efa17
Oscar Rosas <Oscar@macs-mbp.gateway.2wire.net>
2017-04-16 00:04:14 -0500

    Update necesario

--- Geometria/Punto.java
+++ Geometria/Punto.java
@@ -1,22 +1,27 @@
-import java.util.*;													//Librerias
+import java.util.*;															//Librerias
 
 public class Punto{
-	private int x, y;												//Atributos
+	private int x, y;														//Atributos
 	private char nombre;
 
-	Punto(int a, int b, char letra) {nombre = letra; x = a; y = b;}	//Constructor base
-	Punto(int num, char letra){this(num, num, letra);} 				//Constructor de ayuda
-	Punto(char letra){this(0,0, letra);} 							//Constructor de ayuda
-	Punto(){this(0,0, 'p');} 										//Constructor de ayuda
-	Punto(Punto P, char letra){this(P.x, P.y, letra);} 				//Constructor de copia
+	Punto(int a, int b, char letra) {nombre = letra; x = a; y = b;}			//Constructor base
+	Punto(int a, int b){this(a, b, ' ');} 									//Constructor de ayuda
+	Punto(int num, char letra){this(num, num, letra);} 						//Constructor de ayuda
+	Punto(char letra){this(0,0, letra);} 									//Constructor de ayuda
+	Punto(){this(0,0, 'p');} 												//Constructor de ayuda
+	Punto(Punto P, char letra){this(P.x, P.y, letra);} 						//Constructor de copia
 
-	public int getX(){return x;}									//Setters
-	public int getY(){return y;}									//Setters
+	public int getX(){return x;}											//Setters
+	public int getY(){return y;}											//Setters
 
 	public String toString(){return (nombre+"("+this.x+", "+this.y+")");}	//Mostramos la info
 
-	public double Distancia() {return Math.sqrt((x*x)+(y*y));}		//Distancia
-	public int cuadrante() {										//Nos dice en que cuadrante estoy
+	public double distancia() {return distancia(new Punto());}				//Distancia				
+	public double distancia(Punto A) {
+		return Math.sqrt(((A.x-x)*(A.x-x))+((A.y-y)*(A.y-y)));
+	}
+		
+	public int cuadrante() {												//Nos dice en que cuadrante estoy
 		if(x>0 && y>0) return 1;	
 		if(x<0 && y>0) return 2;									
 		if(x<0 && y<0) return 3;
--- Geometria/Rectangulo.java
+++ Geometria/Rectangulo.java
@@ -4,8 +4,8 @@ public class Rectangulo{
 	private Punto a, b;													//Atributos
 	private String nombre;												//Atributos
 
-	Rectangulo(Punto a, Punto b, String nombre) {						//Constructor base
-		this.a = a; this.b = b; this.nombre = nombre;					//Asignar valores
+	Rectangulo(Punto P1, Punto P2, String nombre) {						//Constructor base
+		a = P1; b = P2; this.nombre = nombre;							//Asignar valores
 	}
 	Rectangulo(int x1, int x2, int y1, int y2, String nombre){			//Constructor pro
 		this(new Punto(x1, y1, '1'), new Punto(x2, y2, '2'), nombre);	//todo en una linea :v
@@ -13,19 +13,65 @@ public class Rectangulo{
 	Rectangulo(Rectangulo R, String nombre){this(R.a, R.b, nombre);}	//Constructor de copia
 
 
+	public String toString(){return (nombre+"["+this.a+","+this.b+"]");}//Mostramos la info
+
 	public Punto getX(){return a;}										//Setters
 	public Punto getY(){return b;}										//Setters
 
+	Rectangulo getUnion(Rectangulo R){
+		int[] arrayX = {a.getX(), b.getX(), R.a.getX(), R.b.getX()};	//Dame todas las x
+		int[] arrayY = {a.getY(), b.getY(), R.a.getY(), R.b.getY()};	//Dame todas las y
 
+		Arrays.sort(arrayX);											//Ordename esta (de la manera mas respetable posible)
+		Arrays.sort(arrayY);											//Ordename esta (de la manera mas respetable posible)
 
+		String nuevoNombre = "Union "+nombre+" + "+R.nombre+": ";		//Creemos un nuevo nombre
+		Punto nuevoX = new Punto(arrayX[0], arrayY[0]);					//El primer punto es la mas pequeña de todo
+		Punto nuevoY = new Punto(arrayX[3], arrayY[3]);					//y el segundo es la mas grande
 
-	public static void main(String[] args) {
-		Punto q = new Punto(1, 1, 'q');
-		Punto r = new Punto(3, -1, 'r');
-
-		System.out.println(q.cuadrante());
-		System.out.println(r.cuadrante());
+		return new Rectangulo(nuevoX, nuevoY, nuevoNombre);				//Ahora regresame ese objeto, andale!
 	}
+
+	Rectangulo getInteseccion(Rectangulo R){
+		int[] arrayX = {a.getX(), b.getX(), R.a.getX(), R.b.getX()};	//Dame todas las x
+		int[] arrayY = {a.getY(), b.getY(), R.a.getY(), R.b.getY()};	//Dame todas las y
+
+		Arrays.sort(arrayX);											//Ordename esta (de la manera mas respetable posible)
+		Arrays.sort(arrayY);											//Ordename esta (de la manera mas respetable posible)
+
+		String nuevoNombre = "Interseccion "+nombre+" + "+R.nombre+": ";//Creemos un nuevo nombre
+		Punto nuevoX = new Punto(arrayX[1], arrayY[1]);					//El primer punto es la mas pequeña de todo
+		Punto nuevoY = new Punto(arrayX[2], arrayY[2]);					//y el segundo es la mas grande
+
+		return new Rectangulo(nuevoX, nuevoY, nuevoNombre);				//Ahora regresame ese objeto, andale!
+	}
+
+	boolean estaDentro(Punto P){
+		int[] arrayX = {a.getX(), b.getX(), P.getX()};					//Dame todas las x
+		int[] arrayY = {a.getY(), b.getY(), P.getY()};					//Dame todas las y
+
+		Arrays.sort(arrayX);											//Ordename esta (de la manera mas respetable posible)
+		Arrays.sort(arrayY);											//Ordename esta (de la manera mas respetable posible)
+
+		if (P.getX() == arrayX[1] && P.getY() == arrayY[1])
+			return true;
+		return false;
+	}
+
+	double getArea(){
+		double area = ((a.getX()-b.getX())*(a.getY()-b.getY()));
+		if (area < 0) return (area * -1);
+		return area;
+	}
+
+	boolean compara(Rectangulo R){
+		if (R.getArea() == this.getArea()) return true;
+		return false;
+	}
+
+	void moverRectangulo(Punto P1, Punto P2){a = P1; b = P2;}
+
+	int cuadrante(){return a.cuadrante();}
 }
 
 
